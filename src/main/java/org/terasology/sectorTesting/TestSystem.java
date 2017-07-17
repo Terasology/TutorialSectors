5b98dac29c3fcb950744bf635fae5f65759e56a1
Vizaxo <vizaxo@mailfence.com>
2017-07-17 16:29:35 +0100

    Testing various things with sector entities

--- src/main/java/org/terasology/sectorTesting/TestSystem.java
+++ src/main/java/org/terasology/sectorTesting/TestSystem.java
@@ -18,21 +18,19 @@ package org.terasology.sectorTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.context.Context;
-import org.terasology.entitySystem.Component;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.entity.internal.EntityInfoComponent;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.logic.players.PlayerCharacterComponent;
 import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
-import org.terasology.protobuf.EntityData;
 import org.terasology.registry.In;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class TestSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
@@ -45,6 +43,8 @@ public class TestSystem extends BaseComponentSystem implements UpdateSubscriberS
     @In
     private Context context;
 
+    private int timer = 0;
+
     @Override
     public void update(float delta) {
 
@@ -55,29 +55,41 @@ public class TestSystem extends BaseComponentSystem implements UpdateSubscriberS
             } else if (entity == EntityRef.NULL) {
                 logger.error("Entity is EntityRef.NULL");
             }
-            //logger.info("Entity {} has scope {}", entity.getId(), entity.getScope());
-            //logger.info("Component? {}", entity.getComponent(TestComponent.class));
 
-            //logger.info("==== scope {}", entity.getScope());
             TestComponent comp = entity.getComponent(TestComponent.class);
             if (comp != null) {
-                logger.info("==== {}, scope {}", comp.testValue, entity.getScope());
+                logger.info("== val {} scp {} aw: {} act: {}", comp.testValue, entity.getScope(), entity.isAlwaysRelevant(), entity.isActive());
+                LocationComponent loc = entity.getComponent(LocationComponent.class);
+                if (loc != null) {
+                    logger.info("id: {}; x: {} y: {} z: {}", entity.getId(), loc.getWorldPosition().x, loc.getWorldPosition().y,
+                            loc.getWorldPosition().z);
+                }
+            }
+        }
+        logger.info("==========");
+
+        if ((timer++ % 200) == 0 && timer <= 1000) {
+            for (EntityRef player : entityManager.getEntitiesWith(PlayerCharacterComponent.class)) {
+                LocationComponent loc;
+                if ((loc = player.getComponent(LocationComponent.class)) != null) {
+                    Random rand = new Random();
+                    EntityRef e;
+                    if (rand.nextBoolean()) {
+                        e = entityManager.create();
+                    } else {
+                        e = entityManager.createSectorEntity();
+                    }
+                    e.addComponent(new LocationComponent(loc.getWorldPosition()));
+                    e.addComponent(new TestComponent());
+                    e.setAlwaysRelevant(true);
+                }
             }
         }
 
-        List<Class<? extends Component>> l = new ArrayList<>();
-        l.add(TestComponent.class);
-
-        for (EntityRef entity : entityManager.getEntitiesWith(TestComponent.class)) {
-            TestComponent component = entity.getComponent(TestComponent.class);
-            component.testValue++;
-            logger.info("{}: Entity {}", component.testValue, entity.getId());
-        }
     }
 
     @ReceiveEvent
     public void onPlayerSpawn(OnPlayerSpawnedEvent event, EntityRef player) {
-        //SectorManager sectorManager = entityManager.getSectorManager();
 
         //Global entity
         EntityRef entity = entityManager.create();
@@ -86,27 +98,17 @@ public class TestSystem extends BaseComponentSystem implements UpdateSubscriberS
         entity.addComponent(testComp);
         logger.info("Created {} entity with id ", entity.getId());
 
-        //entityManager.getSectorPool().createEntityWithId(entity.getId(), new ArrayList<Component>());
-        //entityManager.getSectorPool().create(entityManager.getPrefabManager().getPrefab("noprefab"));
-
-
         //Sector entity
         EntityRef entity2 = entityManager.createSectorEntity();
         TestComponent testComp2 = new TestComponent();
-        testComp2.testValue = -10000;
+        testComp2.testValue = 1000;
         entity2.addComponent(testComp2);
         logger.info("Created {} entity with id ", entity2.getId());
-
-
-        /*
-                sectorManager.moveToPool(entity.getId(), sectorManager);
-        logger.info("Are the entities in the same sector? Should be true: " +
-                sectorManager.inSameSector(entity.getId(), entity2.getId()));
-
-        EntityRef entity3 = entityManager.createSectorEntity();
-
-        logger.info("Are the entities in the same sector? Should be true: " +
-                sectorManager.inSameSector(entity3.getId(), entity2.getId()));
-                */
     }
+
+    @Override
+    public void initialise() {
+
+    }
+
 }
