ed754878a2931bdb23121a9631daea534c0e0c69
DarkWeird <foxflameless@gmail.com>
2021-08-30 11:29:30 +0300

    chore(tutorial): migrate tutorial modules with engine package movement

--- src/main/java/org/terasology/sectorTesting/TestComponent.java
+++ src/main/java/org/terasology/sectorTesting/TestComponent.java
@@ -15,7 +15,7 @@
  */
 package org.terasology.sectorTesting;
 
-import org.terasology.entitySystem.Component;
+import org.terasology.engine.entitySystem.Component;
 
 public class TestComponent implements Component {
     public float floatValue;
